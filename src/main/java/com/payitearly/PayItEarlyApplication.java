1ea353ec87a8be07d13bc7c0741050eaec04fafb
JGP345 <jorgeprias345@gmail.com>
2025-02-18 15:34:16 -0600

    took out test

--- src/main/java/com/payitearly/PayItEarlyApplication.java
+++ src/main/java/com/payitearly/PayItEarlyApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.payitearly;
+package com.payitearly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
