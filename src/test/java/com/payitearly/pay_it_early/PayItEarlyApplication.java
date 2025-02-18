c36eeb54ca67b84a4073b0226d70ea81218aca3b
JGP345 <jorgeprias345@gmail.com>
2025-02-18 15:02:43 -0600

    main app

---
+++ src/test/java/com/payitearly/pay_it_early/PayItEarlyApplication.java
@@ -0,0 +1,10 @@
+package main.java.com.payitearly;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayItEarlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayItEarlyApplication.class, args);
+    }
+}
